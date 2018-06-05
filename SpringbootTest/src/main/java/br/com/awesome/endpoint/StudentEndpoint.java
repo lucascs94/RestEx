cabad197dfa9635db388cd578c2c5ba675362703
schefer <schefer@FLB1A1B3G5.americas.cpqcorp.net>
2018-06-05 13:28:39 -0300

    alterações no endpoint

--- SpringbootTest/src/main/java/br/com/awesome/endpoint/StudentEndpoint.java
+++ SpringbootTest/src/main/java/br/com/awesome/endpoint/StudentEndpoint.java
@@ -3,10 +3,13 @@ package br.com.awesome.endpoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.awesome.error.CustomErrorType;
@@ -20,12 +23,12 @@ public class StudentEndpoint {
 	public StudentEndpoint() {
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<?> listAll() {
 		return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
 		Student s = new Student();
 		s.setId(id);
@@ -37,22 +40,22 @@ public class StudentEndpoint {
 		return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
 	public ResponseEntity<?> save(@RequestBody Student student) {
 		Student.studentList.add(student);
 		return new ResponseEntity<>(student, HttpStatus.OK);
 	}
 	
-	@RequestMapping(method = RequestMethod.DELETE)
+	@DeleteMapping
 	public ResponseEntity<?> delete(@RequestBody Student student) {
 		Student.studentList.remove(student);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
-	@RequestMapping(method = RequestMethod.PUT)
+	@PutMapping
 	public ResponseEntity<?> update(@RequestBody Student student) {
 		Student.studentList.remove(student);
 		Student.studentList.add(student);
-		return new ResponseEntity<>(student, HttpStatus.OK);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
