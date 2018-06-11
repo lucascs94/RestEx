77666a21c509727765faf398b74d72f8def107eb
schefer <schefer@FLB1A1B3G5.americas.cpqcorp.net>
2018-06-11 16:15:50 -0300

    aula 12 completada - 11/06/2018

--- SpringBootDemo/src/main/java/br/com/demo/endpoint/StudentEndpoint.java
+++ SpringBootDemo/src/main/java/br/com/demo/endpoint/StudentEndpoint.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -51,6 +52,7 @@ public class StudentEndpoint {
 	}
 
 	@PostMapping
+	@Transactional
 	public ResponseEntity<?> save(@RequestBody Student student) {
 		return new ResponseEntity<>(studentDAO.save(student), HttpStatus.CREATED);
 	}
