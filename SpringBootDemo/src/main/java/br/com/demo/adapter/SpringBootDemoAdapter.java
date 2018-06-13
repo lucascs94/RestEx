b21f820aa6d04512f205f149f1645c73c484d7f2
schefer <schefer@FLB1A1B3G5.americas.cpqcorp.net>
2018-06-13 14:02:29 -0300

    Aula 16 completada - 13/06/2018

--- SpringBootDemo/src/main/java/br/com/demo/adapter/SpringBootDemoAdapter.java
+++ SpringBootDemo/src/main/java/br/com/demo/adapter/SpringBootDemoAdapter.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.SortHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +20,10 @@ public class SpringBootDemoAdapter implements WebMvcConfigurer {
 		phmar.setFallbackPageable(PageRequest.of(0, 5));
 		argResolvers.add(phmar);
 		
+		//não bombou
+		SortHandlerMethodArgumentResolver shmar = new SortHandlerMethodArgumentResolver();
+		shmar.setFallbackSort(new Sort(Direction.ASC, "name"));
+		argResolvers.add(shmar);
 	}
 
 }
